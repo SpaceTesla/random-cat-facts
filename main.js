c16946f398256908fb503b67685185f80f13e064
Shivansh Karan <96942282+SpaceTesla@users.noreply.github.com>
2024-02-17 16:34:37 +0530

    Update to axios

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ let url = "https://catfact.ninja/fact";
 let generate = document.querySelector(".generate");
 
 let getCatFact = async () => {
-  let res = await fetch(url);
-  let factObj = await res.json();
-
-  let para = document.createElement("p");
-  para.innerHTML = factObj.fact;
-  para.classList.add("para");
-  document.querySelector("body").append(para);
+  try {
+    let res = await axios.get(url);
+    console.log("Fact Found");
+    return res.data.fact;
+  } catch (err) {
+    console.log("Request Failed:", err);
+    return "No Fact Found";
+  }
 };
 
-generate.addEventListener("click", getCatFact);
+generate.addEventListener("click", async () => {
+  let fact = await getCatFact();
+  let para = document.createElement("p");
+  para.innerHTML = fact;
+  para.classList.add("para");
+  document.querySelector("body").append(para);
+});
